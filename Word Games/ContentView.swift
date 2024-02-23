76df6f2301a4e58fd37f5279fd4eaa5cb160311e
rahulcb03 <rahulcb03@gmail.com>
2024-02-23 12:23:08 -0500

    Initial Commit

---
+++ Word Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Word Games
+//
+//  Created by Rahul B on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Word Games/Word_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_GamesApp.swift
+//  Word Games
+//
+//  Created by Rahul B on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
